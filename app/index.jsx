ed7530bc0a84d30498a3130bd6a751570199badd
Ribaud Hugo <hugo.ribaud@cap4lab.com>
2024-04-17 12:39:49 +0200

    project setup

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex flex-1 items-center justify-center bg-white'>
+      <Text className='text-3xl'>Aora</Text>
+      <StatusBar style='auto' />
+      <Link
+        href='/profile'
+        className='text-blue-500 hover:text-blue-700'
+      >
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
